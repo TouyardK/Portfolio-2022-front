f28082910d7715afd11f32cd62437e566eafad01
TouyardK <touyard.kevin@outlook.fr>
2021-09-13 18:21:51 +0200

    Add photo and title

--- portfolio-2022-front/src/HomePage/Header.jsx
+++ portfolio-2022-front/src/HomePage/Header.jsx
@@ -1,13 +1,24 @@
 import styled from 'styled-components';
 import NavBar from '../NavBar/NavBar';
+import Photo from '../HomePage/Photo';
+import TitlePhoto from './TitlePhoto';
 
 export const StyledHeader = styled.div`
 `;
 
+const Blank = styled.div`
+height: 8vh;
+`;
+
 export default function Header () {
     return (
+    <>
         <StyledHeader>
             <NavBar />
         </StyledHeader>
+                <Blank />
+            <Photo />
+        <TitlePhoto />
+    </>
     );
 }
---
+++ portfolio-2022-front/src/HomePage/Photo.jsx
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+import photoK from '../img/photoK.jpg';
+
+const StyledContainer = styled.div`
+display: flex;
+justify-content: center;
+`;
+
+const StyledContainerPhoto = styled.div`
+margin-bottom: 48px;
+min-height: 280px;
+min-width: 280px;
+height: 280px;
+width: 280px;
+max-height: 280px;
+max-height: 280px;
+`;
+
+const StyledPhoto = styled.img`
+position: relative;
+z-index: 3;
+width: 100%;
+height: 100%;
+border-radius: 50%;
+filter: drop-shadow(0 0 0.95rem orange);
+`;
+
+
+export default function Photo () {
+    return (
+    <>
+    <StyledContainer>
+        <StyledContainerPhoto>
+            <StyledPhoto src={photoK} alt="Kevin" />
+        </StyledContainerPhoto>
+    </StyledContainer>
+    </>
+    );
+}
