13b73df6d64ce3151bc4996e44cc9f9fdc89eaef
TouyardK <touyard.kevin@outlook.fr>
2021-10-17 11:15:21 +0200

    MaJ Soft Skills

--- portfolio-2022-front/src/HomePage/SoftSkills.jsx
+++ portfolio-2022-front/src/HomePage/SoftSkills.jsx
@@ -16,19 +16,20 @@ flex-direction: column;
 background-color: black;
 border-radius: 25px;
 width: 150vh;
-height: auto;
+height: 50vh;
 line-height: 0.5;
 padding-left: 20vh;
 `;
 
 const StyledH1 = styled.h1`
 color: white;
-letter-spacing: 5px;
+letter-spacing: 10px;
 font-size: 8vh;
 `;
 
 const StyledH2 = styled.h2`
-color: grey;
+color: #999999;
+letter-spacing: 8px;
 font-size: 4vh;
 `;
 
@@ -55,7 +56,7 @@ export default function SoftSkills () {
                 <StyledTitleBox>
                     <StyledH2>Soft Skills</StyledH2>
                     <StyledH1>JavaScript developper</StyledH1>
-                    <StyledH1>Webflow Partner.</StyledH1>
+                    <StyledH1>Front-end Designer.</StyledH1>
                 </StyledTitleBox>
             </StyledTitleContainer>
         </>
