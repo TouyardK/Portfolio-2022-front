164fceb1de886c1dc5ee2540f8eadf94d2798931
TouyardK <touyard.kevin@outlook.fr>
2021-10-20 12:01:51 +0200

    Update style Navbar

--- portfolio-2022-front/src/Styles/StyledNavBar.jsx
+++ portfolio-2022-front/src/Styles/StyledNavBar.jsx
@@ -5,7 +5,7 @@ export const StyledNavBar = styled.nav`
 display: flex;
 background-color: black;
 justify-content: center;
-border-bottom: solid 3px #A58DE3;
+border-bottom: solid 1px white;
 width: auto;
 height: auto;
 `;
@@ -13,5 +13,21 @@ height: auto;
 export const StyledLink = styled(Link)`
 display: flex;
 text-decoration: none;
-color: white;
+padding: 15px;
+  background-color: rgba(0, 0, 0, 0);
+  border: 1px rgba(255,255,255,.15) solid;
+  box-shadow: 0px 3px 10px rgba(255,255,255,.1);
+  color: #ffffff;
+  width: max-content;
+  float: left;
+  margin-left: 15px;
+  position: relative;
+  transition: box-shadow .5s ease 0s, top .5s ease 0s, border .5s ease 0s, color .5s ease 0s;
+  top: 0;
+&:hover {
+  cursor: pointer;
+  box-shadow: 0px 6px 15px #ff00007f;
+  top: -3px;
+  border: 1px #d900ff4c solid;
+}
 `;
