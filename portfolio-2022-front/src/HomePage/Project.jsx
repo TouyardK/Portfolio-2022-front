c1084ab2c3ce9168cea0f19cdeb615368f647fc1
TouyardK <touyard.kevin@outlook.fr>
2021-10-20 13:13:26 +0200

    Update Size carousel and screen

--- portfolio-2022-front/src/HomePage/Project.jsx
+++ portfolio-2022-front/src/HomePage/Project.jsx
@@ -19,7 +19,7 @@ letter-spacing: 1vh;
 text-align: center;
   text-transform: uppercase;
   background: linear-gradient(91.36deg, #ECA658 0%, #F391A6 13.02%, #E188C3 25.52%, #A58DE3 37.5%, #56ABEC 49.48%, #737EB7 63.02%, #C8638C 72.92%, #DD5D57 84.38%, #DF6C51 97.92%);
-  letter-spacing: 5px;
+  letter-spacing: 10px;
   -webkit-background-clip: text;
   background-clip: text;
   -webkit-text-fill-color: transparent;
@@ -30,7 +30,7 @@ export default function Project () {
     return (
         <>  
             <ProjectContainer>
-                <TitleProject>Mes Projets.</TitleProject>
+                <TitleProject>Projets.</TitleProject>
                 <ProjectBox />
             </ProjectContainer>
         </>
