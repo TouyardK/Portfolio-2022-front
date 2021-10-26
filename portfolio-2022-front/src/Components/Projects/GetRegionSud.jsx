178fa0b47fdb34a7c6282a487112a113a48b55fa
TouyardK <touyard.kevin@outlook.fr>
2021-10-26 15:47:31 +0200

    Fill modal with projects details

--- portfolio-2022-front/src/Components/Projects/GetRegionSud.jsx
+++ portfolio-2022-front/src/Components/Projects/GetRegionSud.jsx
@@ -4,6 +4,9 @@ import styled from 'styled-components';
 import Modal from '../Modal/Modal';
 import regionsud from '../../Assets/regionsud.png';
 import useModal from '../Modal/useModal';
+import rs2 from '../../Assets/regionsud2.png';
+import rs3 from '../../Assets/regionsud3.png';
+import rs4 from '../../Assets/regionsud4.png';
 
 
 export const BodyProjects = styled.div`
@@ -33,6 +36,41 @@ background-image: linear-gradient(120deg, #eb4f56, #9d66e9);
 border-radius: 35px;
 width: 20vh;
 height: 5vh;
+`;
+
+export const ModalBody = styled.div`
+display: flex;
+flex-direction: column;
+width: 100%;
+height: 55vh;
+padding: 50px;
+`;
+
+export const ContainerImg = styled.div`
+display: flex;
+justify-content: space-around;
+width: 100%;
+height: 35vh;
+`;
+
+export const ContainerText = styled.div`
+display: flex;
+width: 100%;
+height: 10vh;
+`;
+
+
+export const ModalText = styled.p`
+text-align: center;
+`;
+
+export const ModalImg = styled.img`
+height: 35vh;
+width: 48vh;
+&:hover{
+    transform: scale(1.80);
+    transition: all 2s;
+  }
 `;
 
 
@@ -65,9 +103,21 @@ export default function GetRegionSud () {
                   <Modal
                   isOpen={isProjectShowed}
                   hide={toggleInProject}
-                  title="Hello"
+                  title="Région Sud"
                   >
-
+                      <ModalBody>
+                        <ContainerImg>
+                          <ModalImg src={rs2} alt="rs2" />
+                          <ModalImg src={rs3} alt="rs3" />
+                          <ModalImg src={rs4} alt="rs4" />
+                        </ContainerImg>
+                        <ContainerText>
+                             <ModalText>
+                            Conception d'un calculateur d'impact environnemental, qui contient un questionnaire avec des recommandations qui varient en fonction des réponses, un système de notation en temps réel, un
+                            espace administrateur et user.
+                             </ModalText>
+                          </ContainerText>
+                      </ModalBody>
                   </Modal>
                 </BodyProjects>
                 ))}
