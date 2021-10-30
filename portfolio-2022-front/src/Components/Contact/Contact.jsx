977e4f0a4b421e874385839612f608697ab6d346
TouyardK <touyard.kevin@outlook.fr>
2021-10-30 18:05:53 +0200

    Add a contact section and add a scrolling animation

---
+++ portfolio-2022-front/src/Components/Contact/Contact.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import styled from "styled-components";
+import ContactTitle from "./ContactTitle";
+import { Animator, ScrollContainer, ScrollPage, batch, FadeIn, StickyIn, ZoomIn } from "react-scroll-motion";
+
+const ContactMainContainer = styled.div`
+display: block;
+background-color: black;
+height:auto;
+`;
+
+
+const ZoomInScrollOut = batch(StickyIn(), FadeIn(), ZoomIn());
+
+export default function ContactContainer () {
+    return(
+        <>
+            <ContactMainContainer>
+                <ScrollContainer>
+                    <ScrollPage page={6}>
+                        <Animator animation={ZoomInScrollOut}>
+                            <ContactTitle />
+                        </Animator>
+                    </ScrollPage>        
+                </ScrollContainer>
+            </ContactMainContainer>
+        </>
+    );
+}
