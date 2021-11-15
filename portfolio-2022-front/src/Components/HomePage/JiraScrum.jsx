0d1303d4753f812ad830548f7c39b1f940dfad71
TouyardK <touyard.kevin@outlook.fr>
2021-11-15 12:18:20 +0100

    JiraScrumContainer responsive

--- portfolio-2022-front/src/Components/HomePage/JiraScrum.jsx
+++ portfolio-2022-front/src/Components/HomePage/JiraScrum.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from "styled-components";
 import agile from '../../Assets/agile.jpg';
+import { device } from '../Styles/device';
 
 
 const JiraScrumContainer = styled.div`
@@ -9,6 +10,11 @@ justify-content: center;
 align-items: center;
 background-color: black;
 height: 60vh;
+@media ${device.mobileS}{
+    flex-direction: column;
+    align-items: center;
+    height: 55vh;
+  }
 `;
 
 const ContainerImgAndText = styled.div`
@@ -17,6 +23,12 @@ border-radius: 28px;
 height: 50vh;
 width: 160vh;
 letter-spacing: -.045em;
+@media ${device.mobileS}{
+    flex-direction: column;
+    align-items: center;
+    height: 38vh;
+    width: 40vh;
+  }
 `;
 
 const ImgResponsive = styled.img`
@@ -25,6 +37,10 @@ width: 75vh;
 border-radius: 15px;
 border: 1px solid hsla(0,0%,51.4%,.16);
 box-shadow: 0 0 10px 5px rgb(163, 83, 255);
+@media ${device.mobileS}{
+height: 20vh;
+width: 40vh;
+  }
 `;
 
 const RightCol = styled.div`
@@ -32,6 +48,8 @@ display: flex;
 justify-content: center;
 align-items: center;
 width: 50%;
+@media ${device.mobileS}{
+  }
 `;
 
 const LeftCol = styled.div`
@@ -40,12 +58,19 @@ flex-direction: column;
 justify-content: center;
 align-items: center;
 width: 50%;
+@media ${device.mobileS}{
+height: auto;
+width: auto;
+  }
 `;
 
 const TitleResponsive = styled.h2`
 color: white;
 font-size: 3vh;
 font-family: "SF Pro Display","SF Pro Icons","Helvetica Neue","Helvetica","Arial",sans-serif;
+@media ${device.mobileS}{
+font-size: 2.5vh;
+  }
 `;
 
 const TextResponsive = styled.p`
@@ -53,6 +78,9 @@ color: white;
 font-size: 2vh;
 text-align: center;
 font-family: "SF Pro Display","SF Pro Icons","Helvetica Neue","Helvetica","Arial",sans-serif;
+@media ${device.mobileS}{
+font-size: 1.70vh;
+  }
 `;
 
 const Line = styled.div`
@@ -60,6 +88,9 @@ border: solid 1px;
 background-image: linear-gradient(120deg, #4e68ff, #b726f0);
 width: 17vh;
 height: 1px;
+@media ${device.mobileS}{
+width: 15vh;
+  }
 `;
 
 const LineTop = styled.div`
@@ -67,6 +98,9 @@ border: solid 1px;
 background-image: linear-gradient(120deg, #4e68ff, #b726f0);
 width: 45vh;
 height: 1px;
+@media ${device.mobileS}{
+width: 30vh;
+  }
 `;
 
 
